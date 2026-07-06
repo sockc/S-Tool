7a3706120b0b33f6feac857e2fbc988a98160f1b
sockc <86035855+sockc@users.noreply.github.com>
2026-07-06 14:23:12 +0800

    Update SystemUiDualSimHook.java

--- app/src/main/java/com/sockc/unicomhook/SystemUiDualSimHook.java
+++ app/src/main/java/com/sockc/unicomhook/SystemUiDualSimHook.java
@@ -1,4 +1,3 @@
-$code = @'
 package com.sockc.unicomhook;
 
 import android.app.Application;
@@ -19,13 +18,17 @@ public class SystemUiDualSimHook implements IXposedHookLoadPackage {
 
         log("loaded " + lpparam.packageName);
 
-        XposedBridge.hookAllMethods(Application.class, "attach", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) {
-                Context ctx = (Context) param.args[0];
-                log("attach ok, package=" + ctx.getPackageName());
-            }
-        });
+        try {
+            XposedBridge.hookAllMethods(Application.class, "attach", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) {
+                    Context ctx = (Context) param.args[0];
+                    log("attach ok, package=" + ctx.getPackageName());
+                }
+            });
+        } catch (Throwable t) {
+            log("hook Application.attach failed: " + t);
+        }
     }
 
     private static void log(String msg) {
@@ -33,7 +36,3 @@ public class SystemUiDualSimHook implements IXposedHookLoadPackage {
         Log.i(TAG, msg);
     }
 }
-'@
-
-New-Item -ItemType Directory -Force .\app\src\main\java\com\sockc\unicomhook | Out-Null
-Set-Content -Path .\app\src\main\java\com\sockc\unicomhook\SystemUiDualSimHook.java -Value $code -Encoding UTF8
